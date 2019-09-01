44ebd8470ba02ce604f4d68be92e4fa31d70d15f
cristian <cristianberlingo@gmail.com>
2019-09-01 17:25:44 -0300

    Initial commit

---
+++ API/Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodreadsCloneAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        // GET api/books
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Book1", "Book2" };
+        }
+
+        // GET api/books/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return string.Format("Book {0}", id);
+        }
+
+        // POST api/books
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/books/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/books/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
